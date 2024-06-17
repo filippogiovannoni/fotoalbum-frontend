cc9d005109bb6b9cacb30fa751c4adc791dc0362
filippogiovannoni <filippogiovannonii@gmail.com>
2024-06-17 09:26:35 +0200

    fixed layout

--- src/App.vue
+++ src/App.vue
@@ -47,6 +47,7 @@ export default {
 
   filterByCategory() {
     this.loading = true
+    this.photos = ''
       if (this.selectedCategory) {
         const filteredUrl = `${this.base_api_url + this.photos_endpoint}?category_id=${this.selectedCategory}`
         axios.get(filteredUrl)
@@ -77,56 +78,45 @@ mounted() {
 <template>
 
 <div class="container mt-5">
+        <h1 class="text-center mb-4">Photo Gallery</h1>
 
-    <h1 class="text-center mb-4">Photo Gallery</h1>
-
-    <div class="mb-3">
-      <label class="form-label">Filter by category</label>
-      <select class="form-select" name="selectedCategory" id="selectedCategory" v-model="selectedCategory" @change="filterByCategory()">
-        <option selected disabled>Select one</option>
-        <option v-for="category in categories" :value="category.id">{{ category.name }}</option>
-      </select>
-    </div>
-
-    <div class="container text-center">
-
-<div class="loader fs-3 align-items-center" v-if="loading">
-  <i class="fa-solid fa-spinner fa-spin"></i>
-   <span class="d-block">Loading photos</span>
-</div>
-
-</div>
-
-    
-
-  <div class="row">
-      <div class="col-md-4" v-for="photo in photos" v-if="photos && photos.length >= 1" >
-        <div class="card mb-4">
-          <img :src="base_api_url + '/storage/' + photo.image_url" class="card-img-top" alt="Titolo della foto 1">
-          <div class="card-body">
-            <h5 class="card-title">{{ photo.title }}</h5>
-            <p class="card-text">{{ photo['category'].name }}</p>
-          </div>
+        <div class="mb-3">
+            <label class="form-label">Filter by category</label>
+            <select class="form-select" name="selectedCategory" id="selectedCategory" v-model="selectedCategory" @change="filterByCategory()">
+                <option selected disabled>Select one</option>
+                <option v-for="category in categories" :value="category.id">{{ category.name }}</option>
+            </select>
         </div>
-      </div>
-    <div v-else-if="photos.length == 0 && loading == false">
 
-      <div class="container">
-        <div class="row">
-            <div class="col">
-                <div class="mt-3 text-center fs-4">
-                    <i class="fa fa-camera" aria-hidden="true"></i>
-                    <h4>No Photos Available</h4>
-                    <p class="fs-6">Try with a different category!</p>
-                  </div>
-                </div>
-              </div>
+        <div class="container text-center">
+            <div class="loader fs-3 align-items-center" v-if="loading">
+                <i class="fa-solid fa-spinner fa-spin"></i>
+                <span class="d-block">Loading photos</span>
             </div>
-            
-          </div>
         </div>
-        
-      </div>
+
+        <div class="row">
+            <div class="col-md-4" v-for="photo in photos" v-if="photos && photos.length >= 1">
+                <div class="card mb-4">
+                    <img :src="base_api_url + '/storage/' + photo.image_url" class="card-img-top" alt="Titolo della foto 1">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ photo.title }}</h5>
+                        <p class="card-text">{{ photo['category'].name }}</p>
+                    </div>
+                </div>
+            </div>
+            <div v-else-if="photos.length == 0 && loading == false">
+                <div class="col">
+                    <div class="mt-3 text-center fs-4">
+                        <i class="fa fa-camera" aria-hidden="true"></i>
+                        <h4>No Photos Available</h4>
+                        <p class="fs-6">Try with a different category!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <style>
