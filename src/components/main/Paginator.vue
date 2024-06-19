a7dc447cdbc9935c8d172891df900e06a6d213cf
filippogiovannoni <filippogiovannonii@gmail.com>
2024-06-19 14:07:01 +0200

    add Paginator component

---
+++ src/components/main/Paginator.vue
@@ -0,0 +1,43 @@
+<script>
+import { store } from '../../store.js'
+export default {
+    name: 'Paginator',
+    data() {
+        return {
+            store,
+        }
+    }
+}
+</script>
+
+
+<template>
+    <nav class="mt-5" aria-label="Page navigation">
+        <ul class="pagination justify-content-center   ">
+            <li class="page-item" :class="{ 'disabled': !link.url, 'active': link.active }"
+                v-for="link in store.photos.links">
+                <button class="page-link" :href="link.url" type="button" @click="store.getPhotos(link.url)">
+
+                    <span class="text-purple" v-html="link.label"></span>
+
+                </button>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<style scoped>
+.page-item.active .page-link {
+    background-color: #8F6593;
+    border-color: #8F6593;
+    color: white;
+
+    & span {
+        color: white;
+    }
+}
+
+.text-purple {
+    color: #AEA4BF;
+}
+</style>
--- src/components/main/PhotoGallery.vue
+++ src/components/main/PhotoGallery.vue
@@ -2,9 +2,10 @@
 import { store } from '../../store.js'
 import PhotoElement from './PhotoElement.vue'
 import NoResult from './NoResult.vue'
+import Paginator from './Paginator.vue'
 export default {
     name: 'PhotoGallery',
-    components: { PhotoElement, NoResult },
+    components: { PhotoElement, NoResult, Paginator },
     data() {
         return {
             store,
@@ -28,33 +29,8 @@ export default {
         </div>
     </div>
 
-    <nav class="mt-5" aria-label="Page navigation">
-        <ul class="pagination justify-content-center   ">
-            <li class="page-item" :class="{ 'disabled': !link.url, 'active': link.active }"
-                v-for="link in store.photos.links">
-                <button class="page-link" :href="link.url" type="button" @click="store.getPhotos(link.url)">
-
-                    <span class="text-purple" v-html="link.label"></span>
-
-                </button>
-            </li>
-        </ul>
-    </nav>
+    <Paginator></Paginator>
 
 </template>
 
-<style scoped>
-.page-item.active .page-link {
-    background-color: #8F6593;
-    border-color: #8F6593;
-    color: white;
-
-    & span {
-        color: white;
-    }
-}
-
-.text-purple {
-    color: #AEA4BF;
-}
-</style>
+<style scoped></style>
